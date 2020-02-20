883ae2829f39dca3fad4d573ed34949e82025a32
Aleksandr Loschev <alex@loschev.com>
2020-02-20 16:30:23 +0300

    Избавился от двойного цикла

--- 03_homework1/03_for.py
+++ 03_homework1/03_for.py
@@ -9,11 +9,6 @@
 * Посчитать и вывести средний балл по всей школе.
 * Посчитать и вывести средний балл по каждому классу.
 """
-list_for_school = [{'school_class': '4a', 'scores': [3, 4, 5, 5, 4]},
-                   {'school_class': '4б', 'scores': [5, 5, 5, 5, 5]},
-                   {'school_class': '4а', 'scores': [4, 4, 4, 4, 4]},
-                   {'school_class': '4г', 'scores': [2, 2, 3, 4, 4]}]
-all_summary = []
 
 
 def main(list_from_school):
@@ -21,10 +16,14 @@ def main(list_from_school):
         print(f'Для класса {element["school_class"]}:')
         print(f"Оценки в списке: {element['scores']}")
         print(f"Средняя оценка класса {sum(element['scores']) / len(element['scores'])}\n")
-        for numbers in element['scores']:
-            all_summary.append(numbers)
+        all_summary.append(sum(element['scores']) / len(element['scores']))
     print(f'Средняя оценка по школе: {sum(all_summary) / len(all_summary)}')
 
 
 if __name__ == "__main__":
+    list_for_school = [{'school_class': '4a', 'scores': [3, 4, 5, 5, 4]},
+                       {'school_class': '4б', 'scores': [5, 5, 5, 5, 5, 5]},
+                       {'school_class': '4а', 'scores': [4, 4, 4, 4, 4]},
+                       {'school_class': '4г', 'scores': [2, 2, 3, 4, 4]}]
+    all_summary = []
     main(list_for_school)
