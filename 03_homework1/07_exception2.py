02485cc98e19c70c1b5fcb2e29ede4ac52985cde
Aleksandr Loschev <alex@loschev.com>
2020-02-20 15:26:50 +0300

    Переписал, чтобы работало со float

--- 03_homework1/07_exception2.py
+++ 03_homework1/07_exception2.py
@@ -13,21 +13,28 @@
 """
 
 
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
 def get_summ(num_one, num_two):
-    num_one = str(num_one)
-    num_two = str(num_two)  # Не уверен, на счет правильности решения. Возможно это костыль. Но работает:)
-    if num_one.isdigit() and num_two.isdigit():
-        calculate = int(num_one) + int(num_two)
-        return calculate
+    if isinstance(num_one, (int, float)) and isinstance(num_two, (int, float)):
+        return num_one + num_two
+    elif is_number(num_one) and is_number(num_two):
+        return float(num_one) + float(num_two)
     elif ValueError:
         return 'Переменная не является цифровым значением'
-    else:
-        return 0
 
 
 if __name__ == "__main__":
     print(get_summ(2, 2))
-    print(get_summ(3, "3"))
-    print(get_summ("4", "4"))
+    print(get_summ(3.0, "3"))
+    print(get_summ("3", 3.0))
+    print(get_summ("4", "4.0"))
+    print(get_summ("4", "-4"))
     print(get_summ("five", 5))
     print(get_summ("six", "шесть"))
