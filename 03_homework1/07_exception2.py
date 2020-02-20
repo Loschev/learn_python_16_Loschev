6d7be510289d9502ba7117db5aba37871b0662da
Aleksandr Loschev <alex@loschev.com>
2020-02-20 16:29:53 +0300

    Оптимизировал код

--- 03_homework1/07_exception2.py
+++ 03_homework1/07_exception2.py
@@ -22,9 +22,7 @@ def is_number(string):
 
 
 def get_summ(num_one, num_two):
-    if isinstance(num_one, (int, float)) and isinstance(num_two, (int, float)):
-        return num_one + num_two
-    elif is_number(num_one) and is_number(num_two):
+    if is_number(num_one) and is_number(num_two):
         return float(num_one) + float(num_two)
     elif ValueError:
         return 'Переменная не является цифровым значением'
@@ -34,6 +32,7 @@ if __name__ == "__main__":
     print(get_summ(2, 2))
     print(get_summ(3.0, "3"))
     print(get_summ("3", 3.0))
+    print(get_summ("2.5", 2.5))
     print(get_summ("4", "4.0"))
     print(get_summ("4", "-4"))
     print(get_summ("five", 5))
